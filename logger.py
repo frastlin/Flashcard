6ebd266d05406ef61b07eed860d65d4edfcf2a81
Brandon <brandonkeithbiggs@gmail.com>
2015-02-22 12:11:13 +0100

    Added a logger module for printing and logging at different levels

---
+++ logger.py
@@ -0,0 +1,26 @@
+#This is the module that deals with error messages and output
+log_file = "Flashcard_log.txt"
+
+def log(message, level=0):
+	"""Call this function in order to print to the log"""
+	if level == 0:
+		log_write(message)
+
+
+def log_write(message):
+	"""Will write to the file and print to the console"""
+	print(message)
+	with open(log_file, "a") as f:
+		f.write(message + "\n")
+
+def start(message=None, clear=False):
+	"""Will date and add the message to the logging file"""
+	import time
+	current_string = "Started at:\n"
+	current_string += time.strftime("%A %B %d at %I:%M%p", time.localtime())
+	if message:current_string += "\n" + message
+	if clear:
+		f = open(log_file, "w")
+		f.close()
+	log(current_string)
+
---
+++ my_decks/italian_cards.py
@@ -0,0 +1,178 @@
+body parts
+
+we put it in a container
+la mettiamo nel contenitore
+
+microwave
+microonde
+
+frozen
+surgilato
+
+expresso maschine
+caffetteria
+
+coffi maschine
+macchina per  caffe
+
+nife block
+porta coltello  
+
+arm
+il braccio  
+hand
+la mano
+leg
+la gamba
+foot
+il piede
+elbow
+il gomito
+finger
+il dito  
+fingers
+le dita    
+knee
+il ginocchio
+knees
+le ginocchia
+nose
+il naso
+ear
+l'orecchio
+ears
+le orecchie
+back
+la schiena
+neck
+il collo
+sholder
+la spalla
+mouth
+la bocca
+lips
+le labbra
+stomach
+la pancia
+chest
+il petto
+head
+la testa
+face
+la faccia
+
+I have a stomach ake
+mi fa male la pancia
+my feet hert
+mi fanno male i piedi    
+I have a tooth aich
+me fano male i denti
+
+volleyball
+pallavolo
+baskitball
+pallacanestro
+Racecar driving
+automobilismo
+scating
+pattinaggio
+motorcycling
+motociclismo
+swimming
+nuoto
+socker
+calcio
+ski
+sci
+
+title:when to use conjunctive
+
+Spero che
+Yes
+Tutti dicono che
+Yes
+Non so se
+Yes
+Credo che
+Yes
+Mi sembra che
+Yes
+Penso che
+Yes
+È possibile che 
+Yes
+È importante che
+Yes
+È necessario che
+Yes
+È inutile che
+Yes
+È meglio che/se
+Yes
+Sono contenta che
+Yes
+Se pensate che
+Yes
+Mi fa piacere che
+Yes
+Mi dispiace che
+Yes
+voglio che
+Yes
+
+È chiaro che
+No
+secondo me
+No
+mi hanno detto che
+No
+sicuramente
+No
+per me
+No
+Sono sicura che  
+No
+so che
+No
+Sono certo che  
+No
+non lo so
+No
+È vero che 
+no
+
+title:conjunctive irregular verbs
+
+essere
+sia
+avere
+abbia
+dare
+dia
+fare
+faccia
+stare
+stia
+sapere
+sappia
+piacere
+piaccia
+volere
+voglia
+
+andare
+vada, andiate, vadano
+rimanere
+rimanga
+bere
+beva
+dire
+dica
+venire
+venga
+potere
+possa
+dovere
+debba, dobbiate, debbano
+
+
